d0fdb8c170b9dfa50cdffff8a522d21ff1c61b8c
Steven Gualpa <68717276+StevenGualpa@users.noreply.github.com>
2023-06-01 20:15:14 -0500

    version 0.2

---
+++ Assets/Scripts/Next.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Next : MonoBehaviour
+{
+    public bool pasarnivel;
+    public int indicenivel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+                CambiardeNivel(indicenivel);
+        }
+        if (pasarnivel)
+        {
+            CambiardeNivel(indicenivel);
+        }
+    }
+
+    public void CambiardeNivel(int indice)
+    {
+        SceneManager.LoadScene(indice);
+       
+    }
+}
